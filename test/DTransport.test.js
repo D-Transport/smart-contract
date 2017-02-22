e18fe460d07fb45cb68cbc8846a8ce19480ef3a6
Coac <levictor3000@hotmail.fr>
2017-02-23 00:40:40 +0100

    Test adminOnly modifier and added rule eslintrc "arrow-body-style"

--- test/DTransport.test.js
+++ test/DTransport.test.js
@@ -1,9 +1,13 @@
 const DTransport = artifacts.require('./DTransport.sol');
 
 contract('DTransport', (accounts) => {
-  it('should set admin the first account', () => DTransport.deployed().then(instance => instance.admin.call()).then((admin) => {
-    assert.equal(admin, accounts[0], 'admin contract is not the account 1');
-  }));
+  it('should set admin the first account', () => {
+    return DTransport.deployed().then((instance) => {
+      return instance.admin.call();
+    }).then((admin) => {
+      assert.equal(admin, accounts[0], 'admin contract is not the account 1');
+    });
+  });
 
   it('should add a company', () => {
     let instance;
@@ -12,8 +16,20 @@ contract('DTransport', (accounts) => {
     return DTransport.deployed().then((inst) => {
       instance = inst;
       return instance.addCompany(accountTwo, 'companyName', { from: accountOne });
-    }).then(() => instance.companies.call(accountTwo, { from: accountOne })).then((company) => {
+    }).then(() => {
+      return instance.companies.call(accountTwo, { from: accountOne });
+    }).then((company) => {
       assert.equal(company[0], 'companyName', 'Company name is not equal as created');
+    });
+  });
+
+  it('should throw error when being not admin and adding a company', () => {
+    const accountOne = accounts[0];
+    const accountTwo = accounts[1];
+    return DTransport.deployed().then((instance) => {
+      return instance.addCompany(accountOne, 'companyName', { from: accountTwo });
+    }).catch((e) => {
+      assert.isDefined(e, 'transaction should have thrown');
     });
   });
 });
