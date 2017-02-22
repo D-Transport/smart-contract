08e8c97f6db5f565cc1316caf7f4f269dc40ce53
Coac <levictor3000@hotmail.fr>
2017-02-22 14:30:36 +0100

    Added prototype methods and documentation

--- contracts/DTransport.sol
+++ contracts/DTransport.sol
@@ -7,53 +7,107 @@ pragma solidity ^0.4.4;
  */
 contract DTransport {
 
+	/**
+	 * Transport company which provides validation terminal
+	 */
 	struct Company {
 		string name;
 	}
 
+	/**
+	 * Validation terminal used to validate "ticket"
+	 */
 	struct ValidationTerminal {
 		uint location;
 		address company;
 	}
 
+	/**
+	 * "Ticket" validation
+	 */
 	struct Validation {
 		uint date;
 		address terminal;
 	}
 
+	/**
+	 * Authorization given by a terminal to a user to validate
+	 */
 	struct Authorization {
 		uint expiration;
 	}
 
+	/**
+	 * User who uses the transport service
+	 */
 	struct User {
 		mapping (address => Authorization) authorizations;
 		Validation[] validations;
 		address addr; // Needed otherwise "Error: Internal type is not allowed for public state variables.""
 	}
 
+	/**
+	 * Admin is the one who created the contract
+	 * He is the only one able to add companies
+	 * The admin can be changed in the future
+	 */
 	address public admin;
-	mapping(address => ValidationTerminal) public terminals;
-	mapping(address => Company) public companies;
-	mapping(address => User) public users;
 
 	modifier onlyAdmin() {
 		if (msg.sender != admin) throw;
 		_;
 	}
 
-	function addTerminal (address terminal, uint location, address company) onlyAdmin {
-
-	}
+	mapping(address => ValidationTerminal) public terminals;
+	mapping(address => Company) public companies;
+	mapping(address => User) public users;
 
 	function DTransport() {
 		admin = msg.sender;
 	}
 
+
+	/**
+	 * @notice Add a transport company to the system
+	 * @param company the company ethereum address
+	 * @param name the company name
+	 */
+	function addCompany (address company, string name) onlyAdmin {
+
+	}
+
+	/**
+	 * @notice Add a terminal to the system
+	 * @param terminal the terminal ethereum address
+	 * @param location the terminal location
+	 * @param company the company which own the terminal ethereum address
+	 */
+	function addTerminal (address terminal, uint location, address company) {
+
+	}
+
+	/**
+	 * @notice Used by an User to validate to a terminal
+	 * @dev Checks if the the user is authorized then create the entry
+	 * @param terminal the terminal ethereum address
+	 */
 	function validate(address terminal) {
 
 	}
 
-	function inspect() {
+	/**
+	 * @notice Used by an Terminal to give an User authorization to validate
+	 * @param User the user ethereum address
+	 */
+	function giveAuthorization (address User) {
 
 	}
+
+	/**
+	 * @notice Register as an User
+	 */
+	function register() {
+
+	}
+
 }
